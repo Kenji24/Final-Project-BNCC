e154c4e704bab73314f758fa934e9f97ba5f106b
kenzie <ktebenezer@gmail.com>
2024-07-18 18:37:57 +0700

    benerin tampilan

--- resources/views/landing/about.blade.php
+++ resources/views/landing/about.blade.php
@@ -7,6 +7,6 @@
         <h1>About Us</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et vel qui aspernatur ipsum nihil officia. Unde, sint? Voluptas earum laudantium accusantium optio! Eaque officia iure repellendus dolores odit. Laboriosam, distinctio! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione corrupti expedita reiciendis magni perspiciatis at corporis modi iure eos hic, quod blanditiis a sequi, ab qui nihil. Expedita, fuga doloremque!</p>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia consequuntur sint nam sequi hic nulla eum saepe vitae aut iste dolorem, rem nesciunt amet, perferendis sit ipsum corporis, voluptate quis!</p>
-        <p>Thank you for choosing CUNNY STORE!</p>
+        <p>Thank you for choosing CUNNY STORE! =w=</p>
     </div>
 @endsection
